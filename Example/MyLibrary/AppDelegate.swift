e10326b13272b7288dd2f8c8a824c0a27d719409
Bytedance <bytedance@c02cg0xkmd6v.sso.bytedance.com>
2020-05-06 13:08:58 +0800

    Initial commit

---
+++ Example/MyLibrary/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyLibrary
+//
+//  Created by Lilyxxzjzljy on 05/06/2020.
+//  Copyright (c) 2020 Lilyxxzjzljy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/MyLibrary/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyLibrary
+//
+//  Created by Lilyxxzjzljy on 05/06/2020.
+//  Copyright (c) 2020 Lilyxxzjzljy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
